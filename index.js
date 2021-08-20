455c030dd9fee07f947ccdb147d8fc97a4918c80
hilmanski <hilman@hilman.com>
2021-08-20 13:31:45 +0800

    add imaginlive explicitly

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ const deta_project_key = process.env.deta_project_key
 const deta = Deta(deta_project_key);
 const checkJwt = require('./middleware/checkJWT')
 
-app.use(cors())
+app.use(cors({
+    origin: 'https://imagin.live'
+}));
 app.use(express.json())
 
 app.post("/users", async (req, res) => {
